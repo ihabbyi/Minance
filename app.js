0205a584e6ce70ee3e90d0c552445a0aaccb232b
= <=>
2023-09-30 18:50:37 +0300

    Global Error Handling

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const ErrorHandler = require('./controllers/errorHandler');
 const userRoute = require('./routes/userRoute');
 
 const app = express();
@@ -10,5 +12,11 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public/`));
 
 app.use('/api/v1/users', userRoute);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`page ${req.originalUrl} not found`, 404));
+});
+
+app.use(ErrorHandler);
 
 module.exports = app;
